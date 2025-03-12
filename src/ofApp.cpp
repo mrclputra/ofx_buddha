2eb19c24f71efa87b658874ccc578c3d87c17159
Marcel Putra <marcelino.ares.putra@gmail.com>
2025-03-12 12:05:13 +0800

    switch to directional light, use new model

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -16,49 +16,67 @@ void ofApp::setup(){
 	auto light1 = std::make_shared<ofLight>();
 	light1->enable();
 	light1->setDirectional();
-	light1->setPosition(500, 500, 500);
+	light1->setPosition(200, 230, 200);
 	light1->lookAt(glm::vec3(0, 0, 0));
 	light1->setDiffuseColor(ofFloatColor(1, 1, 1));
 	light1->setSpecularColor(ofFloatColor(1.0, 1.0, 1.0));
 	light1->getShadow().setStrength(0.6f);
-	light1->getShadow().setNearClip(100);
-	light1->getShadow().setFarClip(2000);
+	//light1->getShadow().setNearClip(50);
+	//light1->getShadow().setFarClip(500);
 	lights.push_back(light1);
 
-	// - light 2: spotlight
+	// - light 2: directional area light
 	auto light2 = std::make_shared<ofLight>();
 	light2->enable();
-	light2->setSpotlight(60, 20);
-	light2->getShadow().setNearClip(200);
-	light2->getShadow().setFarClip(2000);
-	light2->getShadow().setStrength(0.6f);
-	light2->setPosition(210, 330.0, 750);
-	light2->setAmbientColor(ofFloatColor(0.4));
-	// orient
-	/*glm::quat xq = glm::angleAxis(glm::radians(-30.0f), glm::vec3(1, 0, 0));
-	glm::quat yq = glm::angleAxis(glm::radians(20.0f), glm::vec3(0, 1, 0));
-	spotLight->setOrientation(yq * xq);*/
-	light2->lookAt(ofVec3f(0.0f, 0.0f, 0.0f));
+	light2->setDirectional();
+	light2->setPosition(-300, 200, 0);
+	light2->lookAt(glm::vec3(0, 0, 0));
+	light2->setDiffuseColor(ofFloatColor(.5, .5, .5));
+	light2->setSpecularColor(ofFloatColor(1.0, 1.0, 1.0));
+	light2->getShadow().setStrength(0.3f);
 	lights.push_back(light2);
+
+	// - light 2: spotlight
+	//auto light2 = std::make_shared<ofLight>();
+	//light2->enable();
+	//light2->setSpotlight(60, 20);
+	//light2->getShadow().setNearClip(200);
+	//light2->getShadow().setFarClip(2000);
+	//light2->getShadow().setStrength(0.6f);
+	////light2->setPosition(210, 330.0, 750);
+	//light2->setPosition(100,100,100);
+	//light2->setAmbientColor(ofFloatColor(0.4));
+	//// orient
+	///*glm::quat xq = glm::angleAxis(glm::radians(-30.0f), glm::vec3(1, 0, 0));
+	//glm::quat yq = glm::angleAxis(glm::radians(20.0f), glm::vec3(0, 1, 0));
+	//spotLight->setOrientation(yq * xq);*/
+	//light2->lookAt(ofVec3f(0.0f, 0.0f, 0.0f));
+	//lights.push_back(light2);
 
 	// configure shadows
 	ofShadow::enableAllShadows();
-	ofShadow::setAllShadowBias(0.007);
+	ofShadow::setAllShadowBias(0.002f);
 	ofShadow::setAllShadowNormalBias(-4.0f);
-	ofShadow::setAllShadowTypes(OF_SHADOW_TYPE_PCF_LOW);
+	ofShadow::setAllShadowTypes(OF_SHADOW_TYPE_PCF_HIGH);
 	ofShadow::setAllShadowSampleRadius(4.0f); // softness
+#ifndef TARGET_OPENGLES
+	ofShadow::setAllShadowDepthResolutions(1024, 1024);
+#endif
+
 
 	// configure scene objects
 	boxMesh = ofMesh::box(1, 1, 1, 24, 24, 24);
 
-	model.loadModel("stanford/happy_vrip.ply", true);
+	model.loadModel("happy_buddha_stanford.glb");
+	model.disableMaterials();
 
-	baseMaterial.setDiffuseColor(ofFloatColor(0.8f));
-	baseMaterial.setShininess(60);
-	baseMaterial.setSpecularColor(ofFloatColor(1));
+	baseMaterial.setAmbientColor(ofFloatColor(0.85, 0.16, 0.43) * 0.8);
+	baseMaterial.setDiffuseColor(ofFloatColor(0.34, 0.58, 0.38, 1.0));
+	baseMaterial.setSpecularColor(ofFloatColor(ofFloatColor(1.0, 1.0, 1.0, 1.0)));
+	baseMaterial.setShininess(40);
 
 	bgMaterial.setDiffuseColor(ofFloatColor(0.15));
-	bgMaterial.setShininess(0.0);
+	bgMaterial.setShininess(1.0);
 	bgMaterial.setSpecularColor(ofFloatColor(0.25));
 
 	// configure camera
@@ -134,10 +152,10 @@ void ofApp::draw(){
 		else {
 			light->draw();
 		}*/
-		ofDrawSphere(light->getPosition(), 5);
-		/*if (light->getIsEnabled() && light->getShadow().getIsEnabled()) {
+		ofDrawSphere(light->getPosition(), 4);
+		if (light->getIsEnabled() && light->getShadow().getIsEnabled()) {
 			light->getShadow().drawFrustum();
-		}*/
+		}
 	}
 
 	camera.end();
@@ -159,7 +177,7 @@ void ofApp::draw(){
 }
 
 void ofApp::renderScene() {
-	float etimef = ofGetElapsedTimef();
+	//float etimef = ofGetElapsedTimef();
 
 	// draw base cube, rotate over time
 	/*baseMaterial.begin();
@@ -176,18 +194,19 @@ void ofApp::renderScene() {
 	ofPushMatrix(); {
 		ofTranslate(0, 0, 0);
 		ofRotateDeg(180, 1, 0, 0);
-		ofScale(0.2f, 0.2f, 0.2f);
+		ofRotateY(180);
+		ofScale(0.3f, 0.3f, 0.3f);
 		model.drawFaces();
 	} ofPopMatrix();
 	baseMaterial.end();
 
-	bgMaterial.begin();
+	/*bgMaterial.begin();
 	ofPushMatrix(); {
-		ofTranslate(0, -50, 0);
-		ofScale(1000, 1, 1000);
+		ofTranslate(0, -70, 0);
+		ofScale(200, 4, 200);
 		boxMesh.draw();
 	} ofPopMatrix();
-	bgMaterial.end();
+	bgMaterial.end();*/
 }
 
 //--------------------------------------------------------------
